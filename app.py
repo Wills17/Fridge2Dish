74aaf718c1a4e8f631e936ae7bf98d856545e8e2
Williams Odunayo <williamsodunayo70@gmail.com>
2025-11-01 14:19:37 +0100

    Intergrate Gemini API for generating recipe.

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-# app.py
+# import libraires
 import streamlit as st
 from PIL import Image
 import io
-import json
 import numpy as np
 from detector.infer2 import infer_image
 
+# Streamlit app
 st.set_page_config(page_title="Fridge2Dish", layout="centered")
 
+# Title and description
 st.title("Fridge2Dish — AI Chef from Leftovers 🍳🥦")
 st.write("Upload a photo of your fridge or ingredients and get a recipe suggestion.")
 
+# Image upload
 uploaded = st.file_uploader("Upload fridge/photo", type=["jpg","jpeg","png"])
 if uploaded:
     image = Image.open(uploaded).convert("RGB")
@@ -23,10 +25,11 @@ if uploaded:
 
     with st.spinner("Generating recipe..."):
         
-        # from recipes.templates import generate_recipe
-        from recipes.recipe_online import generate_recipe
+        from recipe_generator.recipe_local import generate_recipe
         recipe = generate_recipe(ingredients)
+        
     st.markdown("### Suggested Recipe")
     st.write(recipe)
 else:
     st.info("Try uploading a clear photo of a few ingredients (eg. eggs, tomato, bread).")
+
