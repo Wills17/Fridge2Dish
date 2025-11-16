df3abbfd850770fc7c9dcfec03646d86a4f07e65
Williams Odunayo <williamsodunayo70@gmail.com>
2025-11-16 18:40:57 +0100

    Minor updates.

--- recipe_generator/recipe_local.py
+++ recipe_generator/recipe_local.py
@@ -11,3 +11,4 @@ def generate_recipe(ingredients):
     prompt = f"Create a short creative recipe using {', '.join(ingredients)}:\n\n"
     result = generator(prompt, max_length=120, num_return_sequences=1, temperature=0.9)
     return result[0]['generated_text'].split("\n")[0]
+
