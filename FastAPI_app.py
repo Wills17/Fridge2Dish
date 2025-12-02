c3ccfe742238166a903d70b2b5761ed35f097fd9
Williams Odunayo <williamsodunayo70@gmail.com>
2025-12-02 09:19:17 +0100

    MInor changes.

--- FastAPI_app.py
+++ FastAPI_app.py
@@ -571,4 +571,5 @@ def health():
 
 # Run app
 if __name__ == "__main__":
-    uvicorn.run("FastAPI_app:app", host="0.0.0.0", port=7860, reload=True)
+    uvicorn.run("FastAPI_app:app", host="0.0.0.0", port=7860)
+    
